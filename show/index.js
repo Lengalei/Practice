f6dea628367a197ba0570d772dbd86f61e25cc09
Lengalei <132375932+Lengalei@users.noreply.github.com>
2023-11-17 00:02:24 +0300

    burger

--- show/index.js
+++ show/index.js
@@ -1,6 +1,10 @@
 let countcontainer = document.querySelector('.count');
 let count = document.querySelector('.count').innerHTML;
-
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+burger.addEventListener('click', () => {
+    nav.classList.toggle("active");
+});
 setInterval(() => {
     if (count <= 0) {
         return;
@@ -9,3 +13,5 @@ setInterval(() => {
     countcontainer.textContent = count;
     console.log(count);
 },1000);
+
+// burger menu
